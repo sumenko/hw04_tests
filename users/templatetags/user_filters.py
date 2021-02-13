3c9c70a483d42f7f56f7346b7d131e457e0932b3
Vladimir Sumenko <3760097@gmail.com>
2021-02-14 02:53:18 +0300

    #4

--- users/templatetags/user_filters.py
+++ users/templatetags/user_filters.py
@@ -14,6 +14,5 @@ def addclass(field, css):
 def uglify(text):
     """ ДеЛаЕт тАк """
     return "".join(
-                   c.upper() if i % 2 else c.lower() 
-                   for i, c in enumerate(text)
-                  )
+        c.upper() if i % 2 else c.lower() for i, c in enumerate(text)
+                   )
